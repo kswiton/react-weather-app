8f55d7548a88dfbddb0c7135df6a475d09e6b52e
kswiton <kswiton@gmail.com>
2022-01-31 17:21:00 +0100

    deleted trash files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './styles/app.module.css';
+import { useState, } from "react";
+import SearchBar from './components/SearchBar';
+import Weather from './components/Weather';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+const [weatherData, setWeatherData] = useState('')
+
+return (
+  <div className={styles.container}>
+    <SearchBar searchLocationProp={(fetchedData) => setWeatherData(fetchedData)}/>
+    <Weather weatherData={weatherData} />
+    
+
+  </div>
   );
 }
 
